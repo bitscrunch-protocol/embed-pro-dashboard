9c38c8fa97c6981df4b781895da2ca09f1252baa
pranesh kamthe <pranesh@bitscrunch.com>
2025-03-18 20:30:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { embedDashboard } from "@superset-ui/embedded-sdk";
 
 function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    const response = async () => {
+      try {
+        const check = await fetch(`${process.env.REACT_APP_API_URL}`, {
+          method: "POST",
+          body: JSON.stringify({
+            dashboardId: process.env.REACT_APP_DASHBOARD_ID,
+            userName: process.env.REACT_APP_PRO_DASHBOARD_USERNAME,
+            password: process.env.REACT_APP_PRO_DASHBOARD_PASSWORD,
+          }),
+        });
+
+        if (!check.ok) {
+          throw new Error(`HTTP error! Status: ${check.status}`);
+        }
+
+        const test = await check.json();
+        setData(test);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    response();
+  }, []);
+
+  useEffect(() => {
+    if (data?.token) {
+      embedDashboard({
+        id: "8faada2b-aa86-4cc6-9749-d83e9068c653",
+        supersetDomain: "https://pro.unleashnfts.com/",
+        mountPoint: document.getElementById("my-superset-container"),
+        fetchGuestToken: () => data?.token,
+        iframeSandboxExtras: [
+          "allow-top-navigation",
+          "allow-popups-to-escape-sandbox",
+          "allow-fullscreen",
+        ],
+      });
+    }
+  }, [data]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        id="my-superset-container"
+        style={{ width: "100%", height: "100%" }}
+      ></div>
     </div>
   );
 }
